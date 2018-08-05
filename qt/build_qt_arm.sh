cd235ef60b8343a214b5d44e597696ffae1e6e5e
liuxin <liuxin01@sdc.icbc.com.cn>
2018-08-05 15:40:01 +0800

    增加对linuxfb的支持

--- qt/build_qt_arm.sh
+++ qt/build_qt_arm.sh
@@ -10,7 +10,7 @@ QT_OUTPUT="/usr/local/qt_output/"
 TSLIB_VERSION=""
 [[ -z ${TSLIB_PATH+x} ]] && TSLIB_PATH="https://github.com/kergoth/tslib/releases/download/1.16-rc2/tslib-1.16-rc2.tar.bz2"
 TSLIB_OUTPUT="/usr/local/tslib/"
-QT_CONFIGURE_OPTS="-prefix $QT_OUTPUT -release -opensource -xplatform linux-arm-gnueabi-g++ -qvfb -no-opengl -no-dbus -no-icu -no-eglfs -no-iconv -I$TSLIB_OUTPUT/include -L$TSLIB_OUTPUT/lib"
+QT_CONFIGURE_OPTS="-prefix $QT_OUTPUT -release -opensource -xplatform linux-arm-gnueabi-g++ -no-opengl -no-icu -no-eglfs -no-iconv -nomake tests -nomake examples -linuxfb -I$TSLIB_OUTPUT/include -L$TSLIB_OUTPUT/lib"
 
 mkdir -p $QT_TOP_DIR && cd $QT_TOP_DIR
 #克隆树莓派官方交叉编译工具
@@ -20,11 +20,13 @@ mv $QT_ARMTOOLS_DIR /usr/local/
 echo "export PATH=/usr/local/$QT_ARMTOOLS_DIR/arm-bcm2708/arm-linux-gnueabihf/bin/:$PATH" > ~/.profile && source ~/.profile
 
 #下载并解压tslib库
-wget -O tslib $TSLIB_PATH && tar xvf tslib && rm tslib
+wget -O tslib $TSLIB_PATH && tar xvf tslib
+rm -rf tslib
 TSLIB_VERSION=`ls | grep tslib | awk 'NR==1{print $1}'`
 
 #下载并解压QT
-wget -O qt-everywhere $QT_EVERYWHERE && tar xvf qt-everywhere && rm qt-everywhere
+wget -O qt-everywhere $QT_EVERYWHERE && tar xvf qt-everywhere
+rm -rf qt-everywhere
 QT_VERSION=`ls | grep qt-everywhere | awk 'NR==1{print $1}'`
 
 #编译tslib
