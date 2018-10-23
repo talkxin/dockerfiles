12c8537630d987e029c4d4a4e65a487e90ec1be6
liuxin <liuxin01@sdc.icbc.com.cn>
2018-10-23 15:05:22 +0800

    增加一个生成debian基础镜像的方式

--- arm-builddocker/dockercore-arm.sh
+++ arm-builddocker/dockercore-arm.sh
@@ -20,6 +20,12 @@
 # sudo cp 17.05.0-ce-rc1/binary-client/docker* /usr/bin/
 # sudo cp 17.05.0-ce-rc1/binary-daemon/docker* /usr/bin/
 
+#生成ARM镜像方式
+# apt-get install debootstrap
+# debootstrap --arch=armel --foreign stretch fs_debian_stretch https://mirrors.cloud.tencent.com/debian
+# sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C chroot fs_debian_stretch debootstrap/debootstrap --second-stage
+
+
 # 支持依赖安装 gcc-arm-linux-gnueabihf
 # echo "deb http://ftp.de.debian.org/debian sid main" >> /etc/apt/sources.list
 echo "deb-src http://deb.debian.org/debian jessie main" >> /etc/apt/sources.list
